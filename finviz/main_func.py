5c77aa8fe4c6c415403a0f4db3382c15b3a0db6c
Spok <hackershit@yandex.ru>
2023-11-26 17:34:52 +0300

    list index out of range on line to fetch company_details fix

--- finviz/main_func.py
+++ finviz/main_func.py
@@ -54,7 +54,10 @@ class Stock:
         data = {}
 
         data["Ticker"] = title.cssselect('h1[class="js-recent-quote-ticker quote-header_ticker-wrapper_ticker"]')[0].text_content().strip()
-        company_details = title.cssselect('h2[class="quote-header_ticker-wrapper_company"]')[0]
+        try:
+            company_details = title.cssselect('h2[class="quote-header_ticker-wrapper_company text-xl"]')[0]
+        except IndexError:
+            company_details = title.cssselect('h2[class="quote-header_ticker-wrapper_company"]')[0]
         data["Company"] = company_details.text_content().strip()
 
         try:
